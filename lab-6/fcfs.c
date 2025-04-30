50569598d3b4104c9035d58b045ecc4f54c25ec0
Sugam Karki <karkisugam34@gmail.com>
2025-04-30 12:44:23 +0545

    fcfs

---
+++ lab-6/fcfs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int i, j, sum = 0, n;
+  int d[20];
+  int disk;
+  int temp, max;
+  int dloc;
+  printf("Enter number of location ");
+}
